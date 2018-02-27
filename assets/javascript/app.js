e63b8389ce2c1435cf316e16af2e49634c4ef1b9
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-02-27 00:34:18 -0800

    ok

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     var unanswered = 0;
 
     // questions and answers
-    var questions = ["Who was the starting QB of the 1960 NFL Champion Philadelphia Eagles",
+    var question = ["Who was the starting QB of the 1960 NFL Champion Philadelphia Eagles",
         "Who was the last 76ers player to win the NBA's 6th man of the year award?",
         "On 4th and 1 in the 4th quarter of the 2018 super bowl Nick Foles ran a play that resulted in him catching a TD. What is the name of that famous play?",
         "Ben Simmons was selected with the No. 1 overall pick in the NBA draft. Who was taken right after Simmons?",
@@ -18,118 +18,114 @@ $(document).ready(function () {
         "Who are the only two Phillies managers to win World Series?"
     ];
 
-    var answers = ["Norm Van Brocklin", "Aaron Mckie", "Philly Special", "Brandon Ingram", "11", "Syracuse Nationals", "Charlie Manuel and Dallas Green"];
+    var answer = ["Norm Van Brocklin", "Aaron Mckie", "Philly Special", "Brandon Ingram", "11", "Syracuse Nationals", "Charlie Manuel and Dallas Green"];
 
-    var choiceOne = ["Randall Cunningham", "Raja Bell", "TE end-around", "Lonzo Ball", "8", "San Francisco Warriors", "Charlie Manuel and Dallas Green"];
+    var firstChoice = ["Randall Cunningham", "Raja Bell", "TE end-around", "Lonzo Ball", "8", "San Francisco Warriors", "Charlie Manuel and Dallas Green"];
 
-    var choiceTwo = ["Norm Van Brocklin", "Lou Williams", "Philly Special", "Jaylen Brown", "10", "Virginia Squires", "Larry Bowa and Dallas Green"];
+    var secondChoice = ["Norm Van Brocklin", "Lou Williams", "Philly Special", "Jaylen Brown", "10", "Virginia Squires", "Larry Bowa and Dallas Green"];
 
-    var choiceThree = ["Ron Jaworski", "Aaron Mckie", "The Philly Cheesesteak", "Brandon Ingram", "9", "Kentucky Colonels", "Charlie Manuel and Danny Ozark"];
+    var thirdChoice = ["Ron Jaworski", "Aaron Mckie", "The Philly Cheesesteak", "Brandon Ingram", "9", "Kentucky Colonels", "Charlie Manuel and Danny Ozark"];
 
-    var choiceFour = ["Sunny Jurgensen", "Bobbie Jones", "Philly's Delight", "Jamal Murray", "11", "Syracuse Nationals", "Larry Bowa and Danny Ozark"];
+    var fourthChoice = ["Sunny Jurgensen", "Bobbie Jones", "Philly's Delight", "Jamal Murray", "11", "Syracuse Nationals", "Larry Bowa and Danny Ozark"];
 
-
-    // Show & Hide Functions
-    function showGame() {
-        $("#questionsHolder").show();
-        $("#choiceOne").show();
-        $("#choiceTwo").show();
-        $("#choiceThree").show();
-        $("#choiceFour").show();
+    function showHolders() {
+        $("#question-holder").show();
+        $("#choice-holder-1").show();
+        $("#choice-holder-2").show();
+        $("#choice-holder-3").show();
+        $("#choice-holder-4").show();
     }
-
-    function hideGame() {
-        $("#questionsHolder").hide();
-        $("#choiceOne").hide();
-        $("#choiceTwo").hide();
-        $("#choiceThree").hide();
-        $("#choiceFour").hide();
+    function hideHolders() {
+        $("#question-holder").hide();
+        $("#choice-holder-1").hide();
+        $("#choice-holder-2").hide();
+        $("#choice-holder-3").hide();
+        $("#choice-holder-4").hide();
     }
-
     function hideResults() {
-        $("#correctAnswer").hide();
-        $("#incorrectAnswer").hide();
-        $("#unansweredQuestions").hide();
-        $("#restartGame").hide();
+        $("#correct-holder").hide();
+        $("#incorrect-holder").hide();
+        $("#unanswered-holder").hide();
+        $("#restart-holder").hide();
     }
-
-    function showQuestion() {
+    function displayQuestion () {
         hideResults();
-        $("#answers").hide();
-        $("#image").hide();
-        $("#timer").show();
-        showGame();
-        $("#questionsHolder").html(questions[count]);
-        $("#choiceOne").html(choiceOne[count]);
-        $("#choiceTwo").html(choiceTwo[count]);
-        $("#choiceThree").html(choiceThree[count]);
-        $("#choiceFour").html(choiceFour[count]);
-
-        // Hover CSS
-        $("#choiceOne").hover(function () {
-                $(this).css("background-color", "lemonchiffon");
-            },
-            function () {
-                $(this).css("background-color", "lemonchiffon");
-            });
-        $("#choiceTwo").hover(function () {
-                $(this).css("background-color", "lemonchiffon");
-            },
-            function () {
-                $(this).css("background-color", "lemonchiffon");
-            });
-        $("#choiceThree").hover(function () {
-                $(this).css("background-color", "lemonchiffon");
-            },
-            function () {
-                $(this).css("background-color", "lemonchiffon");
-            });
-        $("#choiceFour").hover(function () {
-                $(this).css("background-color", "lemonchiffon");
-            },
-            function () {
-                $(this).css("background-color", "lemonchiffon");
-            });
+        $("#answer-holder").hide();
+        $("#image-holder").hide();
+        $("#time-holder").show();
+        showHolders();
+        $("#question-holder").html(question[count]);
+        $("#choice-holder-1").html(firstChoice[count]);
+        $("#choice-holder-2").html(secondChoice[count]);
+        $("#choice-holder-3").html(thirdChoice[count]);
+        $("#choice-holder-4").html(fourthChoice[count]);
+    
+    // Hover styles
+        $("#choice-holder-1").hover(function() {
+            $(this).css("color", "lemonchiffon");
+        },
+        function(){
+            $(this).css("color", "white");
+        });
+        $("#choice-holder-2").hover(function() {
+            $(this).css("color", "lemonchiffon");
+        },
+        function(){
+            $(this).css("color", "white");
+        });
+        $("#choice-holder-3").hover(function() {
+            $(this).css("color", "lemonchiffon");
+        },
+        function(){
+            $(this).css("color", "white");
+        });
+        $("#choice-holder-4").hover(function() {
+            $(this).css("color", "lemonchiffon");
+        },
+        function(){
+            $(this).css("color", "white");
+        });
     }
-    $("#choiceOne").on("click", checkAnswer)
-    $("#choiceTwo").on("click", checkAnswer)
-    $("#choiceThree").on("click", checkAnswer)
-    $("#choiceFour").on("click", checkAnswer)
+    $("#choice-holder-1").on("click", checkAnswer) 
+    $("#choice-holder-2").on("click", checkAnswer)
+    $("#choice-holder-3").on("click", checkAnswer)
+    $("#choice-holder-4").on("click", checkAnswer)
 
-    // Check Answer Function
+// Check Answer Function
     function checkAnswer() {
 
-        hideGame();
+        hideHolders();
 
-        if ($(this).text() === answer[count]) {
+        if($(this).text() === answer[count]) {
             stopTime();
             isSelected = true;
-            $("#answers").show();
-            $("#answers").html("Right! The answer is: " + answer[count]);
+            $("#answer-holder").show();
+            $("#answer-holder").html("Right! The answer is: " + answer[count]);
             displayImage();
             correct++;
             count++;
-        } else {
+        }
+        else {
             stopTime();
             isSelected = true;
-            $("#answers").show();
-            $("#answers").html("Wrong! The answer is: " + answer[count]);
+            $("#answer-holder").show();
+            $("#answer-holder").html("Wrong! The answer is: " + answer[count]);
             displayImage();
             incorrect++;
             count++;
-        }
+        } 
 
-        checkGameEnd();
+        checkGameEnd();  
     }
 
-    // Chekc End Game Function
+// Chekc End Game Function
     function checkGameEnd() {
-        if (count === question.length) {
-            $("#timer").hide();
+        if(count === question.length) {
+            $("#time-holder").hide();
             showResults();
             count = 0;
             $(".start").show();
-            $(".start").on("click", function () {
+            $(".start").on("click", function() {
                 resetResults();
                 startGame();
             });
@@ -142,93 +138,99 @@ $(document).ready(function () {
 
     function displayTime() {
         time--;
-        $("#timer").html("Time remaining: " + time);
-
-        if (time <= 0) {
-            hideGame();
-            stopTime();
-            $("#answers").show();
-            $("#answers").html("Time is up! The answer is: " + answer[count]);
-            displayImage();
-            unanswered++;
-            count++;
-            checkGameEnd();
-        }
+        $("#time-holder").html("Time remaining: " + time);
+      
+            if(time <= 0) {
+                hideHolders();
+                stopTime();
+                $("#answer-holder").show();
+                $("#answer-holder").html("Time is up! The answer is: " + answer[count]);
+                displayImage();
+                unanswered++;
+                count++;
+                checkGameEnd();
+            }
     }
 
     function startTime() {
         clearInterval(ticker);
         ticker = setInterval(displayTime, 1000);
     }
-
     function stopTime() {
         clearInterval(ticker);
         resetTime();
-        if (count < question.length - 1) {
+        if(count < question.length - 1) {
             setTimeout(startTime, 2000);
-            setTimeout(showQuestion, 3000);
+            setTimeout(displayQuestion, 3000);
         }
     }
 
     resetTime();
 
-    // Display Images With Answer
+// Display Images With Answer
     function displayImage() {
-        if (count === 0) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/tom_marvolo_riddle.jpg">');
-        } else if (count === 1) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/armando_dippet.jpg">');
-        } else if (count === 2) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/helena_ravenclaw.png">');
-        } else if (count === 3) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/merope_gaunt.png">');
-        } else if (count === 4) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/snitch.jpg">');
-        } else if (count === 5) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/phoenix.jpg">');
-        } else if (count === 6) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/lion.jpg">');
-        } else if (count === 7) {
-            $("#image").show();
-            $("#image").html('<img src="assets/images/neville_longbottom.jpg">');
+        if(count === 0) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/QB.jpg">');
+        }
+        else if(count === 1) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/mckie.jpg">');
+        }
+        else if(count === 2) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/PS.jpg">');
+        }
+        else if(count === 3) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/BI.jpg">');
+        }
+        else if(count === 4) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/AI.jpg">');
+        }
+        else if(count === 5) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/syracuse.jpg">');
+        }
+        else if(count === 6) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/CM.jpg">');
+        }
+        else if(count === 7) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/sports.jpg">');
         }
     }
 
-    // Show Results Function   
+ // Show Results Function   
     function showResults() {
-        $("#correctAnswer").show();
-        $("#correctAnswer").html("Correct: " + correct);
-        $("#incorrectAnswer").show();
-        $("#incorrectAnswer").html("Incorrect: " + incorrect);
-        $("#unansweredQuestions").show();
-        $("#unansweredQuestions").html("Unanswered: " + unanswered);
-        $("#restartGame").show();
-        $("#restartGame").html("Click Start button to play again!");
+        $("#correct-holder").show();
+        $("#correct-holder").html("Correct: " + correct);
+        $("#incorrect-holder").show();
+        $("#incorrect-holder").html("Incorrect: " + incorrect);
+        $("#unanswered-holder").show();
+        $("#unanswered-holder").html("Unanswered: " + unanswered);
+        $("#restart-holder").show();
+        $("#restart-holder").html("Click Start above to play again!");
     }
 
-    // Reset Results Function 
+// Reset Results Function 
     function resetResults() {
         correct = 0;
         incorrect = 0;
         unanswered = 0;
     }
 
-    // Start Game Function
+// Start Game Function
     function startGame() {
         $(".start").hide();
         startTime();
-        showQuestion();
+        displayQuestion();
     }
 
-    // Start Game On Click
-    $(".start").on("click", function () {
-        startGame();
-    });
+// Start Game On Click
+  $(".start").on("click", function() {
+    startGame();
+  });
 });
